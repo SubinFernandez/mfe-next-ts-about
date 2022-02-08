a272eaf511ed9547b08b93dcdea5d4d6f6d70753
Subin Fernandez <Subin.Fernandez@gmail.com>
2022-02-08 01:40:16 +0000

    Added host time optional prop

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -2,13 +2,18 @@ import React from 'react'
 
 import styles from './about.module.scss'
 
-const About: React.FC = () => {
+interface AboutProps {
+  hostTime?: string
+}
+
+const About: React.FC<AboutProps> = ({ hostTime }) => {
   return (
     <div className={styles.container}>
       <section className={styles.root}>
         <h1>About Us</h1>
         <p>Hello, I&apos;m the <strong>about</strong> module</p>
         <p>I&apos;m from https://mfe-next-ts-about.vercel.app/</p>
+        {hostTime && <p>Host&apos;s current time: {hostTime}</p>}
       </section>
     </div>
   )
