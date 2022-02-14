2db07290f3b71347359d848ae4ef5578a474ee77
Subin Fernandez <Subin.Fernandez@gmail.com>
2022-02-14 21:32:08 +0000

    Added callback to send the name to the host

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,19 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import styles from './about.module.scss'
 
 interface AboutProps {
   hostTime?: string
+  nameHandler: (name: string) => void
 }
 
-const About: React.FC<AboutProps> = ({ hostTime }) => {
+const About: React.FC<AboutProps> = ({ hostTime, nameHandler }) => {
+  const [name, setName] = useState('')
+
+  const sendName = (n: string) => {
+    setName('')
+    nameHandler(n)
+  }
+
   return (
     <div className={styles.container}>
       <section className={styles.root}>
         <h1>About Us</h1>
         <p>Hello, I&apos;m the <strong>about</strong> module</p>
         <p>I&apos;m from https://mfe-next-ts-about.vercel.app/</p>
+        <hr className={styles.hr}/>
+
+        <h2>Receive message from host</h2>
+        <small className={styles.quote}>This demonstrates data received from the host through props</small>
         {hostTime && <p>Host&apos;s current time: {hostTime}</p>}
+        <hr className={styles.hr}/>
+
+        <h2>Send message to host</h2>
+        <small className={styles.quote}>This demonstrates data sent to the host through callbacks</small>
+        <label className={styles.form_label} htmlFor='txtName'>Let us know your name</label>
+        <input className={styles.form_input} id='txtName' value={name} onChange={(e) => setName(e.target.value)} type='text' placeholder='Your name'></input>
+        <button className={styles.form_submit} onClick={() => sendName(name)}>Send name to the host</button>
       </section>
     </div>
   )
